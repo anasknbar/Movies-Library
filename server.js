23824bbb9a6404e0681470409e3a4c94360eca81
anasknbar <knbaranasgithub@gmail.com>
2024-02-05 21:05:58 +0000

    lab12

--- server.js
+++ server.js
@@ -1,63 +1,94 @@
-'use strict';
-
 const express = require('express') // require(import) express from my node_modules folder
 const app = express() // invoke express
-const port = 3000
+const axios = require('axios');
+require('dotenv').config()
+const port = process.env.movie_PORT
+const apiKey = process.env.API_KEY_MOVIE
 
-const moveData = require('./MoveData/data.json') // import data
 
 
- app.get('/',moveHandler)
+app.get('/search',moviesSearchHandler)
+app.get('/trending',moviesTrendingHandler)
+app.get('/latestTV',latestTVHandler)
+app.get('/certification',moveCerificationHandler)
 
- function moveHandler(req,res){
-  const requiredMove = new Move(moveData.title,moveData.poster_path,moveData.overview)
-  res.json(requiredMove)
- }
-
- app.get('/favorite',favoritePageHandler)
-
- function favoritePageHandler(req,res){
-  res.send('Welcome to Favorite Page')
+function moviesSearchHandler(req,res){
+  let movieName = req.query
+  let url = `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`
+  axios.get(url)
+  .then(result => {
+    console.log(result)
+    res.json(result)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).send('Internal Server Error')
+  })
   
- }
+}
 
-//handling error 
- app.get('*', handle404Erorr) 
-  function handle404Erorr(req,res){
+function moviesTrendingHandler(req,res){
+  let url = `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`
+  axios.get(url)
+  .then(result => {
+    let movieMapArray = result.map(movie => {
+            return new Movie(movie.id,movie.title,movie.release_date,movie.poster_path,movie.overview)
+          })
+          console.log(movieMapArray)
+          res.json(movieMapArray)
+  })
+  .catch(err =>{
+    console.log(err)
+    res.status(500).send('Internal Server Error')
+  })
+}
 
-     res.status(404).send( 
-      `{
+function latestTVHandler(req,res){
+  let url = `https://api.themoviedb.org/3/tv/latest?api_key=${apiKey}`
+  axios.get(url)
+  .then(result => {
+    console.log(result)
+    res.json(result)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).send('Internal Server Error')
+  })
+}
 
-      "status": 404,
-      "responseText": "Sorry, the requested page was not found!"
-  
-      }`
-      
-     )}
+
+function moveCerificationHandler(req,res){
+  let url = `https://api.themoviedb.org/3/certification/movie/list?api_key=${apiKey}`
+  axios.get(url)
+  .then( result => {
+    console.log(result)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).send('Internal Server Error')
+  })
+}
 
 
-
-app.get('/simulate-500-route',handle500Erorr) 
-function handle500Erorr(req,res){
-
-   res.status(500).send(
-    `{
-
-    "status": 500,
-    "responseText": "Sorry, something went wrong"
-
-    }`
-    
-    );}
-
- function Move(title,posterPath,overview){
+function Movie(id,title,release_date,poster_path,overview){
+  this.id = id
   this.title = title
-  this.posterPath = posterPath
+  this.release_date = release_date
+  this.poster_path = poster_path
   this.overview = overview
- }
+}
+
+app.use((req,res,next)=>{
+  res.status(404).send('404 not found')
+})
+  
+app.use((err,req,res,next) => {
+  console.error(err.stack)
+  res.status(500).send('Internal Server Error')
+})
+
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`movie app listening on port ${port}`)
 })
-
